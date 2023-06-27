7477d5221d685d394bee19681c0e93808725267f
Bartosz Szych <bartoszych17@gmail.com>
2023-06-27 19:04:27 +0200

    Moduł 32

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,9 +1,15 @@
 package com.crud.tasks.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+
+
+
+
+@OpenAPIDefinition
 @Configuration
 public class CoreConfiguration {
 
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -39,7 +39,8 @@ public class TaskController {
     public ResponseEntity<Void> deleteTask(@PathVariable long taskId) {
         service.deleteTask(taskId);
         return ResponseEntity.ok().build();
-    }
+
+        }
 
     @PutMapping
     public ResponseEntity<TaskDto> updateTask(@RequestBody TaskDto taskDto) {
